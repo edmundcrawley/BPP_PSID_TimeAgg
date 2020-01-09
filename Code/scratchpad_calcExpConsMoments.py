b46a29c12f71b09e5783da76760d355d751a2977
edmundcrawley <edmundcrawley@gmail.com>
2020-01-09 11:37:46 -0500

    Tests algebra for calculating exponential decay moments

--- Code/scratchpad_calcExpConsMoments.py
+++ Code/scratchpad_calcExpConsMoments.py
@@ -10,9 +10,9 @@ total_obs = num_periods*num_divisions
 max_decay = 100000
 #np.random.seed(seed=3)
 #########################Parameters
-theta = 0.04
-Omega = 10
-ins_tran_test = 1.0
+theta = 0.75
+Omega = 0.25
+ins_tran_test = 0.5
 #########################
 
 #tran_shocks = (var_tran_test*num_divisions)**0.5*np.random.normal(size=total_obs)
@@ -23,12 +23,12 @@ d_tran_inc = np.zeros_like(tran_cons)
 
 
 
-decay = np.exp(-np.arange(total_obs)*theta/num_divisions)
+decay     = np.exp(-np.arange(total_obs)*theta/num_divisions)
 decay_inc = np.exp(-np.arange(total_obs)*Omega/num_divisions)
 
 observed_cons = np.zeros(num_periods)
 for T1 in range(num_periods):
-    observed_cons[T1] =  num_divisions**0.5*np.sum(theta*ins_tran_test/(1.0-np.exp(-theta)) * decay[max(0,(T1+1)*num_divisions-max_decay):(T1+1)*num_divisions] * np.flip(tran_shocks[max(0,(T1+1)*num_divisions-max_decay):(T1+1)*num_divisions],0))
+    observed_cons[T1] =  np.sum(theta*ins_tran_test/(1.0-np.exp(-theta)) * decay[0:min((T1+1)*num_divisions,max_decay)] * np.flip(tran_shocks[max(0,(T1+1)*num_divisions-max_decay):(T1+1)*num_divisions],0))
 
 d_tran_inc_0_1 = np.zeros(num_periods)*np.nan
 d_tran_inc_1_2 = np.zeros(num_periods)*np.nan
@@ -71,46 +71,62 @@ foo = [var_d_tran_inc_0_1,var_d_tran_inc_1_2,var_d_tran_inc_2_infty]
 delta_obs_inc = observed_inc[1:]-observed_inc[:-1]
 delta_obs_cons = observed_cons[1:]-observed_cons[:-1]
 
-simulated_y_cov = [np.nanmean(delta_obs_inc**2), 
+simulated_y_cov = np.array([np.nanmean(delta_obs_inc**2), 
 np.nanmean(delta_obs_inc[1:]*delta_obs_inc[:-1]), 
 np.nanmean(delta_obs_inc[2:]*delta_obs_inc[:-2]), 
 np.nanmean(delta_obs_inc[3:]*delta_obs_inc[:-3]), 
 np.nanmean(delta_obs_inc[4:]*delta_obs_inc[:-4]),
 np.nanmean(delta_obs_inc[5:]*delta_obs_inc[:-5]),
 np.nanmean(delta_obs_inc[6:]*delta_obs_inc[:-6]),
-np.nanmean(delta_obs_inc[7:]*delta_obs_inc[:-7])]
+np.nanmean(delta_obs_inc[7:]*delta_obs_inc[:-7])])
 
-simulated_c_cov = [np.nanmean(delta_obs_cons**2), 
+e1t = np.exp(-theta)
+e2t = np.exp(-2.0*theta)
+e3t = np.exp(-3.0*theta)
+e1o = np.exp(-Omega)
+e2o = np.exp(-2.0*Omega)
+e3o = np.exp(-3.0*Omega)
+theoretical_y_cov = np.array([2.0/((1.0-e1o)**2) - (3-e1o)/(Omega*(1-e1o)),
+1.0/(2*Omega) * (2.0-e1o) - 1.0/(1-e1o)**2 * (1-(1-e1o)/Omega),
+-1.0/Omega*(1-e1o)         +1.0/(2.0*Omega)*(1.0-e2o)         - (2-e1o)*e1o     /(2.0*Omega)*(1.0-e2o) + 1.0/Omega*e1o     *(1.0-e1o)   + (1.0 -e1o)**2/(2.0*Omega)*e2o,
+-1.0/Omega*(1-e1o)*e1o     +1.0/(2.0*Omega)*(1.0-e2o)*e1o     - (2-e1o)*e2o     /(2.0*Omega)*(1.0-e2o) + 1.0/Omega*e2o     *(1.0-e1o)   + (1.0 -e1o)**2/(2.0*Omega)*e3o,
+-1.0/Omega*(1-e1o)*e2o     +1.0/(2.0*Omega)*(1.0-e2o)*e2o     - (2-e1o)*e3o     /(2.0*Omega)*(1.0-e2o) + 1.0/Omega*e3o     *(1.0-e1o)   + (1.0 -e1o)**2/(2.0*Omega)*e3o*e1o,
+-1.0/Omega*(1-e1o)*e3o     +1.0/(2.0*Omega)*(1.0-e2o)*e3o     - (2-e1o)*e1o*e3o /(2.0*Omega)*(1.0-e2o) + 1.0/Omega*e3o*e1o *(1.0-e1o)   + (1.0 -e1o)**2/(2.0*Omega)*e3o*e2o,
+-1.0/Omega*(1-e1o)*e3o*e1o +1.0/(2.0*Omega)*(1.0-e2o)*e3o*e1o - (2-e1o)*e2o*e3o /(2.0*Omega)*(1.0-e2o) + 1.0/Omega*e3o*e2o *(1.0-e1o)   + (1.0 -e1o)**2/(2.0*Omega)*e3o*e3o,
+-1.0/Omega*(1-e1o)*e3o*e2o +1.0/(2.0*Omega)*(1.0-e2o)*e3o*e2o - (2-e1o)*e3o*e3o /(2.0*Omega)*(1.0-e2o) + 1.0/Omega*e3o*e3o *(1.0-e1o)   + (1.0 -e1o)**2/(2.0*Omega)*e3o*e3o*e1o])
+
+simulated_c_cov = np.array([np.nanmean(delta_obs_cons**2), 
 np.nanmean(delta_obs_cons[1:]*delta_obs_cons[:-1]), 
 np.nanmean(delta_obs_cons[2:]*delta_obs_cons[:-2]), 
 np.nanmean(delta_obs_cons[3:]*delta_obs_cons[:-3]), 
 np.nanmean(delta_obs_cons[4:]*delta_obs_cons[:-4]),
 np.nanmean(delta_obs_cons[5:]*delta_obs_cons[:-5]),
 np.nanmean(delta_obs_cons[6:]*delta_obs_cons[:-6]),
-np.nanmean(delta_obs_cons[7:]*delta_obs_cons[:-7])]
+np.nanmean(delta_obs_cons[7:]*delta_obs_cons[:-7])])
 
-simulated_cy_cov = [np.nanmean(delta_obs_cons[3:]*delta_obs_inc[:-3]), 
+theoretical_c_cov = np.array([ins_tran_test**2*theta/(1-e1t),
+                        -ins_tran_test**2*theta/2.0,
+                        -ins_tran_test**2*theta/2.0 * e1t,
+                        -ins_tran_test**2*theta/2.0 * e2t,
+                        -ins_tran_test**2*theta/2.0 * e3t,
+                        -ins_tran_test**2*theta/2.0 * e3t*e1t,
+                        -ins_tran_test**2*theta/2.0 * e3t*e2t,
+                        -ins_tran_test**2*theta/2.0 * e3t*e3t])
+
+simulated_cy_cov = np.array([np.nanmean(delta_obs_cons[3:]*delta_obs_inc[:-3]), 
 np.nanmean(delta_obs_cons[2:]*delta_obs_inc[:-2]), 
 np.nanmean(delta_obs_cons[1:]*delta_obs_inc[:-1]), 
 np.nanmean(delta_obs_cons[:]*delta_obs_inc[:]),
 np.nanmean(delta_obs_cons[:-1]*delta_obs_inc[1:]),
 np.nanmean(delta_obs_cons[:-2]*delta_obs_inc[2:]),
-np.nanmean(delta_obs_cons[:-3]*delta_obs_inc[3:])]
+np.nanmean(delta_obs_cons[:-3]*delta_obs_inc[3:])])
 
-foo2=[np.nanmean(d_tran_inc_0_1[0:-1]*d_tran_inc_1_2[1:]),
-np.nanmean(d_tran_inc_1_2[0:-1]*d_tran_inc_2_3[1:]),
-np.nanmean(d_tran_inc_2_3[0:-1]*d_tran_inc_3_infty[1:])]
-foo2
+theoretical_cy_cov = np.flip(np.array([-ins_tran_test*theta*(1-e1o)/(1-e1t)*e1o/(theta+Omega)*(1-e1t*e1o) + ins_tran_test*theta*(1-e1o)*e2o/(theta+Omega),
+                               -ins_tran_test*theta*(1-e1o)/(1-e1t)    /(theta+Omega)*(1-e1t*e1o) + ins_tran_test*theta*(1-e1o)*e1o/(theta+Omega),
+                               ins_tran_test*theta/((1-e1o)*(1-e1t))* ((2-e1o)/(Omega+theta)*(1-e1o*e1t) - (1-e1t)/theta) + ins_tran_test*theta*(1-e1o)/(theta+Omega),
+                               ins_tran_test*theta/((1.0-e1o)*(1.0-e1t))*((1-e1t)/theta - (1-e1t*e1o)/(theta+Omega)) - ins_tran_test*theta/(1-e1o)*( (2-e1o)/(theta+Omega)*(1-e1o*e1t) - (1-e1t)/theta ) + ins_tran_test*theta*(1-e1o)*e1t/(theta+Omega),
+                               -ins_tran_test*theta/(1-e1o)    *((1-e1t)/theta - (1-e1o*e1t)/(theta+Omega)  ) - ins_tran_test*theta/(1-e1o)*e1t*( (2-e1o)/(theta+Omega)*(1-e1o*e1t) - (1-e1t)/theta ) + ins_tran_test*theta*(1-e1o)*e2t    /(theta+Omega),
+                               -ins_tran_test*theta/(1-e1o)*e1t*((1-e1t)/theta - (1-e1o*e1t)/(theta+Omega)  ) - ins_tran_test*theta/(1-e1o)*e2t*( (2-e1o)/(theta+Omega)*(1-e1o*e1t) - (1-e1t)/theta ) + ins_tran_test*theta*(1-e1o)*e3t    /(theta+Omega),
+                               -ins_tran_test*theta/(1-e1o)*e2t*((1-e1t)/theta - (1-e1o*e1t)/(theta+Omega)  ) - ins_tran_test*theta/(1-e1o)*e3t*( (2-e1o)/(theta+Omega)*(1-e1o*e1t) - (1-e1t)/theta ) + ins_tran_test*theta*(1-e1o)*e3t*e1t/(theta+Omega)]),0)
 
-np.nanmean(d_tran_inc_0_1[0:-1]*d_tran_inc_1_2[1:])+np.nanmean(d_tran_inc_1_2[0:-1]*d_tran_inc_2_3[1:])+np.nanmean(d_tran_inc_2_3[0:-1]*d_tran_inc_3_infty[1:])
 
-
-foo3=[np.nanmean(d_tran_inc_0_1[0:-2]*d_tran_inc_2_3[2:]),
-np.nanmean(d_tran_inc_1_2_a[0:-2]*d_tran_inc_3_4[2:]),
-np.nanmean(d_tran_inc_1_2_b[0:-2]*d_tran_inc_3_4[2:]),
-np.nanmean(d_tran_inc_2_3[0:-2]*d_tran_inc_4_infty[2:])]
-foo3
-np.sum(foo3)
-
-delta_obs_inc
-d_tran_inc_0_1+d_tran_inc_1_2+d_tran_inc_2_infty
